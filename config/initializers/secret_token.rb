4ce47f63c32313e478edfac9c25a7220594222ae
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-06-03 15:51:59 -0700

    hello git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrdTalks::Application.config.secret_token = '6d5a545fdbe2174925e8b643bf5b865879b04117aa15969cd1bb4a1ed03514b0f1b6bc6d9ea37bc95bda56c4febdffeb7542586e8fc9cd440386ed85416ec2fc'
